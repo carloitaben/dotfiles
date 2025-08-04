5be56537235342ab9eed68711aa6ed5a70d8faa5
Carlo <hola.carlodominguez@gmail.com>
2025-08-04 08:28:37 +0200

    sync

--- .config/nvim/lua/fs-oil.lua
+++ .config/nvim/lua/fs-oil.lua
@@ -2,7 +2,9 @@ vim.pack.add({
         { src = "https://github.com/stevearc/oil.nvim" },
 })
 
-require("oil").setup({
+local oil = require("oil")
+
+oil.setup({
         default_file_explorer = true,
         columns = {
                 "icon",
@@ -17,20 +19,35 @@ require("oil").setup({
                 }
         },
         delete_to_trash = false,
+	  skip_confirm_for_simple_edits = true,
         keymaps = {
                 ["."] = { "actions.cd", mode = "n" },
                 ["h"] = { "actions.parent", mode = "n" },
                 ["l"] = { "actions.select", mode = "n" },
         },
+	view_options = {
+		      show_hidden = true,
+		                is_always_hidden = function(name, _)
+            return name == '..' or name == '.git'
+          end,
+},
+	  ssh = {
+    border = "single",
+  },
+  keymaps_help = {
+    border = "single",
+  },
+
 })
 
 vim.api.nvim_create_autocmd("User", {
-        pattern = "OilEnter",
-        callback = vim.schedule_wrap(function(args)
-                local oil = require("oil")
-                if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
-                        oil.open_preview()
-                end
-        end),
+       pattern = "OilEnter",
+       callback = vim.schedule_wrap(function(args)
+               if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+                       oil.open_preview()
+               end
+       end),
 })
+
+vim.api.nvim_set_keymap("n", "<C-S-e>", ":Oil<cr>", { silent = true, noremap = true })
 
