02c199e32edf4ff269d1106854aef544a01a410b
Carlo <carlo@dallonses.com>
2025-08-05 17:12:54 +0200

    sync

--- .config/nvim/lua/fs-tree.lua
+++ .config/nvim/lua/fs-tree.lua
@@ -9,7 +9,48 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
 })
 
+local api = require("nvim-tree.api")
+
 require("nvim-tree").setup({
+    on_attach = function (bufnr)
+        local opts = { buffer = bufnr }
+        api.config.mappings.default_on_attach(bufnr)
+        -- function for left to assign to keybindings
+        local lefty = function ()
+            local node_at_cursor = api.tree.get_node_under_cursor()
+            -- if it's a node and it's open, close
+            if node_at_cursor.nodes and node_at_cursor.open then
+                api.node.open.edit()
+            -- else left jumps up to parent
+            else
+                api.node.navigate.parent()
+            end
+        end
+        -- function for right to assign to keybindings
+        local righty = function ()
+            local node_at_cursor = api.tree.get_node_under_cursor()
+            -- if it's a closed node, open it
+            if node_at_cursor.nodes and not node_at_cursor.open then
+                api.node.open.edit()
+            end
+        end
+        local toggle = function ()
+            local node_at_cursor = api.tree.get_node_under_cursor()
+
+if node_at_cursor.nodes then
+    if node_at_cursor.open then
+        api.node.open.edit()  -- Assuming you want to close the node
+    else
+        api.node.open.edit()
+    end
+end
+        end
+        vim.keymap.set("n", "h", lefty , opts )
+        vim.keymap.set("n", "<Left>", lefty , opts )
+        vim.keymap.set("n", "<Right>", righty , opts )
+        vim.keymap.set("n", "<Space>", toggle , opts )
+        vim.keymap.set("n", "l", righty , opts )
+    end,
 	sort = {
 		sorter = "case_sensitive",
 	},
@@ -88,5 +129,6 @@ require("nvim-tree").setup({
 	},
 	git = {},
 })
+
 
 vim.api.nvim_set_keymap("n", "<D-S-e>", ":NvimTreeFocus<cr>", { silent = true, noremap = true })
