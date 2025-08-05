bdf2f264596ec05adbd2a454584191785bac1ade
Carlo <hola.carlodominguez@gmail.com>
2025-08-06 01:09:21 +0200

    sync

--- .config/nvim/lua/fs-tree.lua
+++ .config/nvim/lua/fs-tree.lua
@@ -6,128 +6,128 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 vim.pack.add({
-	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
+    { src = "https://github.com/nvim-tree/nvim-tree.lua" },
 })
 
 local api = require("nvim-tree.api")
 
 require("nvim-tree").setup({
-    on_attach = function (bufnr)
+    on_attach = function(bufnr)
         local opts = { buffer = bufnr }
         api.config.mappings.default_on_attach(bufnr)
         -- function for left to assign to keybindings
-        local lefty = function ()
+        local lefty = function()
             local node_at_cursor = api.tree.get_node_under_cursor()
             -- if it's a node and it's open, close
             if node_at_cursor.nodes and node_at_cursor.open then
                 api.node.open.edit()
-            -- else left jumps up to parent
+                -- else left jumps up to parent
             else
                 api.node.navigate.parent()
             end
         end
         -- function for right to assign to keybindings
-        local righty = function ()
+        local righty = function()
             local node_at_cursor = api.tree.get_node_under_cursor()
             -- if it's a closed node, open it
             if node_at_cursor.nodes and not node_at_cursor.open then
                 api.node.open.edit()
             end
         end
-        local toggle = function ()
+        local toggle = function()
             local node_at_cursor = api.tree.get_node_under_cursor()
 
-if node_at_cursor.nodes then
-    if node_at_cursor.open then
-        api.node.open.edit()  -- Assuming you want to close the node
-    else
-        api.node.open.edit()
-    end
-end
+            if node_at_cursor.nodes then
+                if node_at_cursor.open then
+                    api.node.open.edit() -- Assuming you want to close the node
+                else
+                    api.node.open.edit()
+                end
+            end
         end
-        vim.keymap.set("n", "h", lefty , opts )
-        vim.keymap.set("n", "<Left>", lefty , opts )
-        vim.keymap.set("n", "<Right>", righty , opts )
-        vim.keymap.set("n", "<Space>", toggle , opts )
-        vim.keymap.set("n", "l", righty , opts )
+        vim.keymap.set("n", "h", lefty, opts)
+        vim.keymap.set("n", "<Left>", lefty, opts)
+        vim.keymap.set("n", "<Right>", righty, opts)
+        vim.keymap.set("n", "<Space>", toggle, opts)
+        vim.keymap.set("n", "l", righty, opts)
     end,
-	sort = {
-		sorter = "case_sensitive",
-	},
-	view = {
-		width = 40,
-	},
-	  system_open =
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 40,
+    },
+    system_open =
     -- identify OS and set OS-specific cmd with args
-    vim.fn.has("mac") == 1
-    and {
-      cmd = "open",
-      args = { "-R" },
-    }
-    or nil,
-	renderer = {
-		icons = {
-			web_devicons = {
-				file = {
-					enable = false,
-				},
-				folder = {
-					enable = false,
-				},
-			},
-			git_placement = "after",
-			modified_placement = "after",
-			hidden_placement = "after",
-			diagnostics_placement = "signcolumn",
-			bookmarks_placement = "signcolumn",
-			padding = {
-				icon = " ",
-				folder_arrow = " ",
-			},
-			symlink_arrow = " ➛ ",
-			show = {
-				file = true,
-				folder = true,
-				folder_arrow = false,
-				git = true,
-				modified = true,
-				hidden = false,
-				diagnostics = true,
-				bookmarks = true,
-			},
-			glyphs = {
-				default = " ",
-				symlink = "",
-				bookmark = "󰆤",
-				modified = "M",
-				hidden = "󰜌",
-				folder = {
-					arrow_closed = "+",
-					arrow_open = "−",
-					default = "+",
-					open = "−",
-					empty = "+",
-					empty_open = "−",
-					symlink = "",
-					symlink_open = "",
-				},
-				git = {
-					unstaged = "·",
-					staged = "✓",
-					unmerged = "",
-					renamed = "➜",
-					untracked = "U",
-					deleted = "",
-					ignored = "◌",
-				},
-			},
-		},
-	},
-	filters = {
-		custom = { ".git", "node_modules", ".vscode" },
-		dotfiles = true,
-	},
-	git = {},
+        vim.fn.has("mac") == 1
+        and {
+            cmd = "open",
+            args = { "-R" },
+        }
+        or nil,
+    renderer = {
+        icons = {
+            web_devicons = {
+                file = {
+                    enable = false,
+                },
+                folder = {
+                    enable = false,
+                },
+            },
+            git_placement = "after",
+            modified_placement = "after",
+            hidden_placement = "after",
+            diagnostics_placement = "signcolumn",
+            bookmarks_placement = "signcolumn",
+            padding = {
+                icon = " ",
+                folder_arrow = " ",
+            },
+            symlink_arrow = " ➛ ",
+            show = {
+                file = true,
+                folder = true,
+                folder_arrow = false,
+                git = true,
+                modified = true,
+                hidden = false,
+                diagnostics = true,
+                bookmarks = true,
+            },
+            glyphs = {
+                default = " ",
+                symlink = "",
+                bookmark = "󰆤",
+                modified = "M",
+                hidden = "󰜌",
+                folder = {
+                    arrow_closed = "+",
+                    arrow_open = "−",
+                    default = "+",
+                    open = "−",
+                    empty = "+",
+                    empty_open = "−",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                git = {
+                    unstaged = "·",
+                    staged = "✓",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "U",
+                    deleted = "",
+                    ignored = "◌",
+                },
+            },
+        },
+    },
+    filters = {
+        custom = { ".git", "node_modules", ".vscode" },
+        dotfiles = false,
+    },
+    git = {},
 })
 
 
