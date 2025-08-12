7d28c3ade69df4dd6432cfb600fcf6fdf2224b4d
Carlo <carlo@dallonses.com>
2025-08-12 12:25:49 +0200

    sync

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,33 +1,38 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.signcolumn = "yes"
-vim.o.wrap = false
-vim.o.swapfile = false
-vim.o.softtabstop = 2
-vim.o.tabstop = 2
-vim.o.smartindent = true
-vim.o.expandtab = true
-vim.o.mouse = 'a'
+vim.o.number          = true
+vim.o.relativenumber  = true
+vim.o.signcolumn      = "yes"
+vim.o.wrap            = false
+vim.o.swapfile        = false
+vim.o.softtabstop     = 2
+vim.o.tabstop         = 2
+vim.o.smartindent     = true
+vim.o.expandtab       = true
+vim.o.mouse           = 'a'
 -- vim.o.showmode = false
-vim.o.scrolloff = 10
+vim.o.scrolloff       = 10
 vim.opt.sidescrolloff = 10
-vim.opt.backup = false
-vim.o.undofile = true
-vim.o.smoothscroll = true
+vim.opt.backup        = false
+vim.o.undofile        = true
+vim.o.smoothscroll    = true
 
 -- Hide command bar
 -- vim.o.cmdheight = 0
 
-vim.o.foldmethod = 'indent'
-vim.o.foldenable = false
+-- Auto select the first entry but don't insert also show additional
+-- information, if available
+vim.o.completeopt     = 'noinsert,menuone,popup'
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.o.foldmethod      = 'indent'
+vim.o.foldenable      = false
+
+-- Case-insensitive searching unless \C or one or more capital letters in the
+-- search term
+vim.o.ignorecase      = true
+vim.o.smartcase       = true
 
 -- vim.o.inccommand = 'split'
-vim.o.splitright = true
-vim.o.splitbelow = true
+vim.o.splitright      = true
+vim.o.splitbelow      = true
 
 
 vim.g.mapleader = " "
